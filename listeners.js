775134efc1391d5abf62323b0e9926105ac6e926
Luke O'Neill <lukeo3210@gmail.com>
2017-10-03 11:26:19 -0400

    Added Idaho resorts and changed zoom on fly to

--- listeners.js
+++ listeners.js
@@ -1,19 +1,24 @@
 function searchResort(event) {
-  var x = event.which || event.keyCode;
-  if(x == 13){
-    //var geoObject = JSON.parse(geoJsonData);
+  let completed = true;
+  var keyboardCode = event.which || event.keyCode;
+  if(keyboardCode == 13){
+    //Get the textbox value from the id
     var inputResort = document.getElementById('searchText').value
     var resortFeatures = [];
     resortFeatures = geoJsonData.features;
+    //loop through geojson data to find a matching resort
     for(i = 0; i < resortFeatures.length; i++){
-      if(resortFeatures[i].properties.name == inputResort){
-        console.log(resortFeatures[i].geometry.coordinates);
+      if(resortFeatures[i].properties.name.toUpperCase() == inputResort.toUpperCase()){
+        completed = false;
         map.flyTo({
-        center: [resortFeatures[i].geometry.coordinates[0],resortFeatures[i].geometry.coordinates[1]],
-        zoom: 9
-    });
+          center: [resortFeatures[i].geometry.coordinates[0],resortFeatures[i].geometry.coordinates[1]],
+          zoom: 12
+        });
         break;
       }
+    }if(completed){
+      alert('Resort not found');
+      completed = true;
     }
   }
 }
